5fb3a4e543bd5fdc8342e30afc4e4d0d6fee05e4
NeyamulOmy <neyamul.omy@gmail.com>
2022-10-26 09:33:25 +0600

    added registration form

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,9 +1,40 @@
 import React from 'react';
-
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
+import './Login.css'
 const Register = () => {
     return (
-        <div>
-            register
+        <div className='form-container mx-auto mt-5'>
+            <h2>Sign up</h2>
+            <Form className='form-container'>
+                <Form.Group className="mb-3" controlId="formBasicText">
+                    <Form.Label>Full Name</Form.Label>
+                    <Form.Control name='name' type="text" placeholder="Full name" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicText">
+                    <Form.Label>Photo URL</Form.Label>
+                    <Form.Control name='photo' type="text" placeholder="Enter photo url" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control name='email' type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control type="password" placeholder="Confirm Password" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Login
+                </Button>
+            </Form>
+            <p>Already have an account? <Link to='../login'>Login</Link></p>
         </div>
     );
 };
