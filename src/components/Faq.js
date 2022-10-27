8156e28969e668c3f9fb37e040ba5179c161e265
NeyamulOmy <neyamul.omy@gmail.com>
2022-10-27 09:06:55 +0600

    added faq

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -2,8 +2,25 @@ import React from 'react';
 
 const Faq = () => {
     return (
-        <div>
-            This is Faq
+        <div className='ms-5 mt-5'>
+            <h4>Frequently Asked Questions:</h4>
+            <hr />
+            <div className='mt-3'>
+                <h5>What is SkillX?</h5>
+                <p>SkillX is popular learning/ teaching platform trusted by millions.</p>
+            </div>
+            <div className='mt-3'>
+                <h5>For whom SkillX is for?</h5>
+                <p>SkillX is for anyone looking to learn/ teach professional skills.</p>
+            </div>
+            <div className='mt-3'>
+                <h5>How expensive is SkillX?</h5>
+                <p>Prices may vary from course to course</p>
+            </div>
+            <div className='mt-3'>
+                <h5>How can I teach on SkillX?</h5>
+                <p>You may have to participite in a few tests first, in order to prove your expertise before teaching.</p>
+            </div>
         </div>
     );
 };
